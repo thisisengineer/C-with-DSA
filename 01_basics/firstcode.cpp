89b085946066c6523fe010b2daaf7bedd07b2336
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-05-04 15:14:09 +0000

    Basic code

--- 01_basics/firstcode.cpp
+++ 01_basics/firstcode.cpp
@@ -1,7 +1,18 @@
+// C++ program to display "Hello World"
+
+// Header file for input output functions
 #include <iostream>
 using namespace std;
+
+// Main() function: where the execution of
+// program begins
 int main()
 {
-    cout << " Hello World";
-    return 0;
+	// Prints hello world
+	cout << "Hello World";
+
+	return 0;
 }
+//#include
+// This is a preprocessor directive. The #include directive tells the compiler to include the content of a file in the source code.
+
