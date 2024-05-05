6df6698620756dd5419b98415fef3c6dc7f0613c
thisisengineer <154666750+thisisengineer@users.noreply.github.com>
2024-05-05 12:33:28 +0000

    cpp code

---
+++ 02_basics/datatypes.cpp
@@ -0,0 +1,32 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+
+    // integer type 
+    int x  = 5;
+    cin >> x;
+    cout << x ;
+    // long
+    long y = 1000;
+    cin >> y;
+    cout << y;
+
+    //long long 
+
+    long long  z = 10000000000000000;
+    cin >> z;
+    cout << z;
+
+    // float 
+
+    float a = 5.5;
+    cin >> a;
+    cout << a;
+
+    // double 
+    
+    double b = 1.274236487926497;
+    cin >> b;
+    cout << b;  
+}
---
+++ 02_basics/user.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+    cin >> x >> y ;
+    cout << "value of x : " <<  x  << " and y is " << y ;
+    return 0;
+}
